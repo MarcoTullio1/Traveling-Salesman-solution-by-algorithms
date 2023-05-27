70e5ba10001200c528f1f28f4ea31bb9feef5dda
Marco Tullio <64559501+MarcoTullio1@users.noreply.github.com>
2023-05-27 01:51:08 -0300

    Execução do algoritmo guloso e comparação com a solução de força bruta, armazenando o tempo médio de cada solução

--- teste.py
+++ teste.py
@@ -3,6 +3,7 @@ import math
 import pandas as pd
 import numpy as np
 from BF import travellingSalesmanProblem
+from greedy import findMinRoute
 import time
 
 class Test:
@@ -11,43 +12,88 @@ class Test:
         return graph
 
     def question_a(self):
-        data = {
+        data_results = {
             'graph_number': [],
             'vertices': [],
-            'solution': [],
+            'brute_force_solution': [],
+            'greedy_solution': [],
+            'greedy_time_execution': []
+        }
+
+        data_bf_execution = {
+            'vertices': [],
             'average_time_execution': []
         }
-        df = pd.DataFrame(data)
+
+        data_greedy_execution = {
+            'vertices': [],
+            'average_time_execution': [],
+            'matched_solutions': []
+        }
+
+        df_results = pd.DataFrame(data_results)
+        df_bf_execution = pd.DataFrame(data_bf_execution)
+        df_greedy_execution = pd.DataFrame(data_greedy_execution)
 
         vertex_inicialization = 5
         graph_number = 1
 
-        for _ in range(1000):
+        brute_force_solutions = []
+        greedy_solutions = []
+        matched_solutions = []
+
+        for _ in range(10):
             n = vertex_inicialization - 1
             graph = self.generate_random_graph(n)
-            average_time_execution, solution = self.test_bf(graph)
-            df = pd.concat([df, pd.DataFrame({'graph_number': [graph_number],
-                                              'vertices': [n],
-                                              'solution': [solution],
-                                              'average_time_execution': [average_time_execution]})])
+            brute_force_solution, brute_force_time = self.test_bf(graph)
+            greedy_solution, greedy_time = self.test_greedy(graph)
+
+            brute_force_solutions.append(brute_force_solution)
+            greedy_solutions.append(greedy_solution)
+            matched_solutions.append(greedy_solution == brute_force_solution)
+
+            df_results = pd.concat([df_results, pd.DataFrame({'graph_number': [graph_number],
+                                                              'vertices': [n],
+                                                              'brute_force_solution': [brute_force_solution],
+                                                              'greedy_solution': [greedy_solution],
+                                                              'greedy_time_execution': [greedy_time]})])
+
+            df_bf_execution = pd.concat([df_bf_execution, pd.DataFrame({'vertices': [n],
+                                                                        'average_time_execution': [brute_force_time]})])
+
+            df_greedy_execution = pd.concat([df_greedy_execution, pd.DataFrame({'vertices': [n],
+                                                                                'average_time_execution': [greedy_time],
+                                                                                'matched_solutions': [greedy_solution == brute_force_solution]})])
+
             vertex_inicialization += 1
             graph_number += 1
 
-        df.to_csv("./brute_force_execution.csv", index=False)
+        df_results.to_csv("./results.csv", index=False)
+        df_bf_execution.to_csv("./brute_force_execution.csv", index=False)
+        df_greedy_execution.to_csv("./greedy_execution.csv", index=False)
+
+        # Armazenar resultados em um arquivo separado
+        results = {
+            'brute_force_solutions': brute_force_solutions,
+            'greedy_solutions': greedy_solutions,
+            'matched_solutions': matched_solutions
+        }
+        np.savez('./results.npz', **results)
 
     def test_bf(self, graph):
-        total_time_execution_size = 0
+        s = 0
+        start_time = time.time()
+        brute_force_solution = travellingSalesmanProblem(graph, s)
+        end_time = time.time()
+        brute_force_time = end_time - start_time
+        return brute_force_solution, brute_force_time
 
-        for _ in range(70):
-            s = 0
-            start_time = time.time()
-            result = travellingSalesmanProblem(graph, s)
-            end_time = time.time()
-            final_time = end_time - start_time
-            total_time_execution_size += final_time
-
-        average_time_execution = decimal.Decimal(total_time_execution_size / 70)
-        return average_time_execution, result
+    def test_greedy(self, graph):
+        start_time = time.time()
+        greedy_solution = findMinRoute(graph)
+        end_time = time.time()
+        greedy_time = end_time - start_time
+        return greedy_solution, greedy_time
 
 if __name__ == "__main__":
     Test().question_a()
