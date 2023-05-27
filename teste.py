ee7bcaed2cd3efcc6bf040d16ea1fece093df5fc
Marco Tullio <64559501+MarcoTullio1@users.noreply.github.com>
2023-05-27 01:29:21 -0300

    Gera 1000 grafos aleatórios, cada um com (N-1) vértices.

--- teste.py
+++ teste.py
@@ -3,11 +3,13 @@ import math
 import pandas as pd
 import numpy as np
 from BF import travellingSalesmanProblem
-from greedy import findMinRoute
 import time
 
-
 class Test:
+    def generate_random_graph(self, n):
+        graph = np.random.randint(0, 10, (n, n))
+        return graph
+
     def question_a(self):
         data = {
             'vertice': [],
@@ -15,26 +17,22 @@ class Test:
         }
         df = pd.DataFrame(data)
 
-        time_execution = 0
         vertex_inicialization = 5
-        start_time = time.time()
 
-        while time_execution < 240:  # 4 minutos = 240 segundos
-            n = np.random.randint(vertex_inicialization, vertex_inicialization + 1)
-            graph = np.random.randint(0, 10, (n, n))
-            average_time_execution = self.test_bf(graph, n)
+        #for _ in range(1000):
+        for _ in range(100):
+            n = vertex_inicialization - 1
+            average_time_execution = self.test_bf(n)
             df = pd.concat([df, pd.DataFrame({'vertice': [n], 'average_time_execution': [average_time_execution]})])
-            end_time = time.time()
-            time_execution = end_time - start_time
-            time_execution = math.floor(time_execution)
             vertex_inicialization += 1
 
         df.to_csv("./brute_force_execution.csv")
 
-    def test_bf(self, graph, n):
+    def test_bf(self, n):
         total_time_execution_size = 0
 
-        for repetition in range(1, 71):
+        for _ in range(70):
+            graph = self.generate_random_graph(n)
             s = 0
             start_time = time.time()
             result = travellingSalesmanProblem(graph, s)
@@ -45,9 +43,5 @@ class Test:
         average_time_execution = decimal.Decimal(total_time_execution_size / 70)
         return average_time_execution
 
-    def test_greedy(self, graph):
-        findMinRoute(graph)
-
-
 if __name__ == "__main__":
-    question_a = Test().question_a()
+    Test().question_a()
