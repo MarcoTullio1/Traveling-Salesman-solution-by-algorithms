84a72ee30e5104fbfe2530b2890e66d055e521a4
Marco Tullio <64559501+MarcoTullio1@users.noreply.github.com>
2023-06-20 01:51:09 -0300

    Ajustes para testar o progrmação dinamica

--- teste.py
+++ teste.py
@@ -5,7 +5,7 @@ import numpy as np
 from BF import brute_force
 from Backtracking import backtracking
 from greedy import greedy
-import threading
+from Dynamic_programming import dynamic_programming
 
 global greedy_execution_time
 global bf_execution_time
@@ -30,6 +30,7 @@ class Test:
                 route_bf, solution_bf, execution_time_bf = brute_force(graph)
                 route_g, solution_g, execution_time_g = greedy(graph)
                 route_bt, solution_bt, execution_time_bt = backtracking(graph)
+                route_dp, solution_dp = dynamic_programming(graph)  # Teste com programação dinâmica
 
                 total_time_bf += execution_time_bf
                 total_time_g += execution_time_g
@@ -49,6 +50,7 @@ class Test:
             'average_time_execution_bf': [],
             'average_time_execution_greedy': [],
             'average_time_execution_backtracking': [],
+            'average_time_execution_dynamic_programming': [],  # Adicione esta coluna
             'same_solution_count': []
         }
         df_results = pd.DataFrame(data_results)
@@ -77,6 +79,12 @@ class Test:
         }
         df_backtracking = pd.DataFrame(data_backtracking)
 
+        data_dynamic_programming = {  # Adicione este bloco
+            'vertice': [],
+            'solution': []
+        }
+        df_dynamic_programming = pd.DataFrame(data_dynamic_programming)
+
         vertex_inicialization = 5
 
         for i in range(5):
@@ -85,6 +93,7 @@ class Test:
             total_time_bf = 0
             total_time_g = 0
             total_time_bt = 0
+            total_time_dp = 0  # Adicione esta variável
 
             for i in range(1000):
                 graph = self.generate_random_graph(vertex_inicialization)
@@ -92,10 +101,12 @@ class Test:
                 route_bf, solution_bf, execution_time_bf = brute_force(graph)
                 route_g, solution_g, execution_time_g = greedy(graph)
                 route_bt, solution_bt, execution_time_bt = backtracking(graph)
+                route_dp, solution_dp = dynamic_programming(graph)  # Teste com programação dinâmica
 
                 total_time_bf += execution_time_bf
                 total_time_g += execution_time_g
                 total_time_bt += execution_time_bt
+                total_time_dp += solution_dp  # Adicione esta linha
 
                 if (solution_bf == solution_g):
                     same_solution_count += 1
@@ -118,14 +129,20 @@ class Test:
                                                                 'route': [route_bt]
                                                                 })])
 
+                df_dynamic_programming = pd.concat([df_dynamic_programming, pd.DataFrame({'vertice': [vertex_inicialization],
+                                                                                          'solution': [solution_dp]
+                                                                                          })])  # Adicione esta linha
+
                 df_greedy.to_csv("./greedy.csv", index=False)
                 df_bf.to_csv("./brute_force.csv", index=False)
-                df_bf.to_csv("./backtracking.csv", index=False)
+                df_backtracking.to_csv("./backtracking.csv", index=False)
+                df_dynamic_programming.to_csv("./dynamic_programming.csv", index=False)  # Adicione esta linha
 
             df_results = pd.concat([df_results, pd.DataFrame({'vertice': [vertex_inicialization],
                                                               'average_time_execution_bf': [total_time_bf / 1000],
                                                               'average_time_execution_greedy': [total_time_g / 1000],
                                                               'average_time_execution_backtracking': [total_time_bt / 1000],
+                                                              'average_time_execution_dynamic_programming': [total_time_dp / 1000],  # Adicione esta coluna
                                                               'same_solution_count': [same_solution_count]})])
 
             print("Grafo de tamanho " +
@@ -138,4 +155,3 @@ if __name__ == "__main__":
     Test().question_a()
     time.sleep(5)
     Test().question_b()
-
